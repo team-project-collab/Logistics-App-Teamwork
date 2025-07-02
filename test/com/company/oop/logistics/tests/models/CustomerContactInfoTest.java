01d9438c86ee0d0703439725e02db0e2dc88b5ec
teodo <teodoragencheva10685@gmail.com>
2025-07-02 11:06:16 +0300

    Added CustomerContactInfoTest

---
+++ test/com/company/oop/logistics/tests/models/CustomerContactInfoTest.java
@@ -0,0 +1,69 @@
+package com.company.oop.logistics.tests.models;
+
+import com.company.oop.logistics.models.CustomerContactInfo;
+import com.company.oop.logistics.models.enums.City;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CustomerContactInfoTest {
+
+    @Test
+    void validCustomerContactInfo_CreatesSuccessfully() {
+        String fullName="Ivan Ivanov";
+        CustomerContactInfo customerContactInfo = new CustomerContactInfo(1,
+                fullName,
+                "+1234567890",
+                "ivan@example.com",
+                City.MEL);
+        Assertions.assertEquals("Ivan Ivanov", customerContactInfo.getFullName());
+        Assertions.assertEquals("+1234567890", customerContactInfo.getPhoneNumber());
+        Assertions.assertEquals("ivan@example.com", customerContactInfo.getEmail());
+        Assertions.assertEquals(City.MEL, customerContactInfo.getAddress());
+    }
+
+    @Test
+    void invalidFullName_ThrowsException() {
+        Assertions.assertThrows(IllegalArgumentException.class, () ->
+                new CustomerContactInfo(1,
+                        "",
+                        "+1234567890",
+                        "ivan@example.com",
+                        City.MEL));
+    }
+
+    @Test
+    void invalidPhoneNumber_ThrowsException() {
+        Assertions.assertThrows(IllegalArgumentException.class, () ->
+                new CustomerContactInfo(1,
+                        "Ivan Ivanov",
+                        "abc",
+                        "ivan@example.com",
+                        City.MEL));
+    }
+
+    @Test
+    void invalidEmail_ThrowsException() {
+        assertThrows(IllegalArgumentException.class, () ->
+                new CustomerContactInfo(1,
+                        "Ivan Ivanov",
+                        "+1234567890",
+                        "invalid-email",
+                        City.MEL)
+        );
+    }
+
+    @Test
+    void nullCity_ThrowsException() {
+        assertThrows(IllegalArgumentException.class, () ->
+                new CustomerContactInfo(1,
+                        "John Doe",
+                        "+1234567890",
+                        "john@example.com",
+                        null)
+        );
+    }
+
+}
---
+++ test/com/company/oop/logistics/tests/models/DeliveryPackageImplTest.java
@@ -0,0 +1,7 @@
+package com.company.oop.logistics.tests.models;
+
+import com.company.oop.logistics.models.DeliveryPackageImpl;
+
+public class DeliveryPackageImplTest {
+
+}
