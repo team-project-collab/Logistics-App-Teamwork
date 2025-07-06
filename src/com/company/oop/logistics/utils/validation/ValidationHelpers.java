db695c0a49b6e142b8c753cacbd010a6e702743e
KristiyanTanchev <kristiyan.tanchev@gmail.com>
2025-07-06 15:41:55 +0300

    Fixes to ValidationHelpers time methods

--- src/com/company/oop/logistics/utils/validation/ValidationHelpers.java
+++ src/com/company/oop/logistics/utils/validation/ValidationHelpers.java
@@ -10,7 +10,8 @@ import java.util.regex.Pattern;
 public class ValidationHelpers {
 
     private static final String INVALID_NUMBER_OF_ARGUMENTS = "Invalid number of arguments. Expected: %d; received: %d.";
-    public static final String TIME_IN_THE_PAST_ERR = "Time for %s cannot be in the past.";
+    private static final String TIME_IN_THE_PAST_ERR = "Time for %s cannot be in the past.";
+    private static final int MINUTES_TOLERANCE = 1;
 
     public static void validateIntRange(int value, int min, int max, String message) {
         if (value < min || value > max) {
@@ -47,13 +48,13 @@ public class ValidationHelpers {
     }
 
     public static void validateTimeAgainstPresent(LocalDateTime time, String field) {
-        if (LocalDateTime.now().isBefore(time)) {
+        if (time.isBefore(LocalDateTime.now().minusMinutes(MINUTES_TOLERANCE))) {
             throw new IllegalArgumentException(String.format(TIME_IN_THE_PAST_ERR, field));
         }
     }
 
     public static void validateTimeAgainstTime(LocalDateTime timeBefore, LocalDateTime timeAfter, String fieldBefore, String fieldAfter) {
-        if (timeBefore.isAfter(timeAfter)) {
+        if (!timeAfter.isAfter(timeBefore)) {
             throw new IllegalArgumentException(String.format("Time for %s cannot be after time for %s.", fieldBefore, fieldAfter));
         }
     }
