de03913049a09b87e52ad986de617a15c21c6b56
KristiyanTanchev <kristiyan.tanchev@gmail.com>
2025-07-13 19:52:41 +0300

    refactor: Removed magic strings from FindRoutesServicingStartAndEndCommand and GetPackageStateCommand

--- src/com/company/oop/logistics/commands/GetPackageStateCommand.java
+++ src/com/company/oop/logistics/commands/GetPackageStateCommand.java
@@ -11,6 +11,12 @@ public class GetPackageStateCommand implements Command {
     private static final int EXPECTED_NUMBER_OF_PARAMETERS = 2;
     private static final String ERROR_PARAMETERS_AMOUNT = String.format("This command requires exactly %d parameters",
             EXPECTED_NUMBER_OF_PARAMETERS);
+    public static final String MESSAGE_PRINT_FORMAT = """
+            Information for package id: %d:
+            %s
+            """;
+    public static final String PACKAGE_ID = "package id";
+    public static final String DATE_TIME = "date time";
 
     private final DeliveryPackageService deliveryPackageService;
     private int packageId;
@@ -27,13 +33,10 @@ public class GetPackageStateCommand implements Command {
         }
         parseParameters(parameters);
 
-        return String.format("""
-                Information for package id: %d:
-                %s
-                """, packageId, deliveryPackageService.getPackageState(packageId, time));
+        return String.format(MESSAGE_PRINT_FORMAT, packageId, deliveryPackageService.getPackageState(packageId, time));
     }
     private void parseParameters(List<String> parameters){
-        packageId = ParsingHelpers.tryParseInteger(parameters.get(0), "package id");
-        time = ParsingHelpers.tryParseLocalDateTime(parameters.get(1), "date time");
+        packageId = ParsingHelpers.tryParseInteger(parameters.get(0), PACKAGE_ID);
+        time = ParsingHelpers.tryParseLocalDateTime(parameters.get(1), DATE_TIME);
     }
 }
